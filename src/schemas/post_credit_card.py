1aa1ef06e1b600fa818101066e5020dc5b473770
Ricardo Gonçalves Ferreira <ricardo_rgf_789@hotmail.com>
2023-11-06 04:10:06 -0300

    Adicionado validacao para o numero do cartao de credito

--- src/schemas/post_credit_card.py
+++ src/schemas/post_credit_card.py
@@ -2,15 +2,23 @@ from typing import Optional
 from datetime import date
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, root_validator
+from creditcard import CreditCard
 
 
 class PostCreditCardSchema(BaseModel):
     exp_date: date
     holder: str
-    number: str
+    number: str = Field(regex="\d+", max_length=20)
     cvv: Optional[str]
 
+    @root_validator()
+    def validate_number(cls, values):
+        number = values["number"]
+        credit_card = CreditCard(number)
+        if not credit_card.is_valid():
+            raise ValueError("Invalid credit card number")
+        return values
 
 class PostCreditCardResponse(BaseModel):
     id: UUID
