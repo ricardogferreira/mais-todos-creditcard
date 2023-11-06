dbceab06cc74b47b36c78a7d1d0a7f2031d60b2a
Ricardo Gonçalves Ferreira <ricardo_rgf_789@hotmail.com>
2023-11-06 11:22:38 -0300

    Removido teste para buscar cartoes

--- tests/routes/test_api_v1_credit_card_viewer.py
+++ tests/routes/test_api_v1_credit_card_viewer.py
@@ -46,26 +46,3 @@ def test_post_api_v1_credit_card_success(mocker, client):
     response_data = response.json()
     assert response.status_code == 201
     assert response_data == {"id": "58b352f2-2ac5-476d-8e14-027f6d247a08"}
-
-
-def test_get_api_v1_credit_card_success(mocker, client):
-    mocker.patch(
-        "src.use_cases.get_credit_card.CreditCard.list",
-        return_value=[
-            CreditCard(
-                id="58b352f2-2ac5-476d-8e14-027f6d247a08",
-                number="gAAAAABlSOmLuuoz8l9KvDcvA6j1xK5a8nXrV7uz9xCZs_9Kzk9uUuwy6QlfPrS62O50Ew8Cz7RpsrQbD161sIRfeBBQ_xpgkHbmQZLKh8ODI5Sf_AWOkAg=",
-                brand="visa",
-                holder="test",
-                exp_date=date(2023, 11, 6),
-            )
-        ],
-    )
-    response = client.get(
-        url="/api/v1/credit-card",
-        headers=HEADERS,
-    )
-
-    response_data = response.json()
-    assert response.status_code == 200
-    assert response_data == []
