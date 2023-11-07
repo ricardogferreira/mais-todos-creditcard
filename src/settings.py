33a07e5a78dc7f1a74153b1dc89ef1fa5cde096e
Ricardo Gonçalves Ferreira <ricardo_rgf_789@hotmail.com>
2023-11-07 10:29:16 -0300

    Parametrizado envfile

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,9 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv(".envfile")
+ENVFILE = os.getenv("ENVFILE", ".envfile")
+
+load_dotenv(ENVFILE)
 
 POSTGRES_CONNECTION_URL = os.getenv("POSTGRES_CONNECTION_URL")
 
